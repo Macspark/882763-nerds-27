844f45b4f0386f1cd53fbe3992e540d8e9d39b8d
Macspark <57904588+Macspark@users.noreply.github.com>
2020-01-25 18:43:42 +0400

    fonts fixed

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 let modalWindow = document.querySelector('.modal-window');
 let modalButton = document.querySelector('.write-us-button');
+let modalButton2 = document.querySelector('.modal-window-x');
+let active = 'modal-window-active';
 
 modalButton.onclick = function() {
-  modalWindow.classList.add('modal-window-active');
+  modalWindow.classList.add(active);
 };
 
-let modalButton2 = document.querySelector('.modal-window-x');
-
 modalButton2.onclick = function() {
-  modalWindow.classList.remove('modal-window-active');
+  modalWindow.classList.remove(active);
 };
