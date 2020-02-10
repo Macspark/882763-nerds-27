804270e03fd1068a54ee610573992f98f092d739
Macspark <57904588+Macspark@users.noreply.github.com>
2020-02-10 11:08:37 +0400

    Update script-modal.js

--- js/script-modal.js
+++ js/script-modal.js
@@ -2,12 +2,22 @@ const modalWindow = document.querySelector('.modal');
 const modalOpenBtn = document.querySelector('.contact__button');
 const modalCloseBtn = document.querySelector('.modal__close');
 const modalActive = 'modal--active';
+let isModalOpen = false;
 
 modalOpenBtn.addEventListener('click', function(event) {
   event.preventDefault()
   modalWindow.classList.add(modalActive);
+  isModalOpen = true;
 });
 
 modalCloseBtn.addEventListener('click', function() {
   modalWindow.classList.remove(modalActive);
+  isModalOpen = false;
+});
+
+document.addEventListener('keydown', function(event) {
+  if (event.keyCode == 27) {
+  modalWindow.classList.remove(modalActive);
+  isModalOpen = false;
+  }
 });
