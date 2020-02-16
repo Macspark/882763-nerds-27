807218ae90ab3091f54602200e70c03ad46d01d6
Macspark <57904588+Macspark@users.noreply.github.com>
2020-02-16 17:56:19 +0400

    w

--- js/script-map.js
+++ js/script-map.js
@@ -1,14 +1,14 @@
 ymaps.ready(function () {
   var map = new ymaps.Map("map", {
-      center: [59.938631, 30.322055],
+      center: [59.938931, 30.322055],
       zoom: 17
   });
 
-  var mapPlacemark = new ymaps.Placemark([59.938631, 30.322055], {}, {
+  var mapPlacemark = new ymaps.Placemark([59.938631, 30.323155], {}, {
     iconLayout: 'default#image',
     iconImageHref: 'img/map-marker.png',
-    iconImageSize: [231, 190],
-    iconImageOffset: [45, -188]
+    iconImageSize: [155, 190],
+    iconImageOffset: [-50, -190]
 });
 
   map.geoObjects.add(mapPlacemark);
