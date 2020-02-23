fbd28e5a803f6b17f77ae50b6233ac91b5597e0a
Macspark <57904588+Macspark@users.noreply.github.com>
2020-02-23 18:38:48 +0400

    w

--- js/script-map.js
+++ js/script-map.js
@@ -1,7 +1,8 @@
 ymaps.ready(function () {
   var map = new ymaps.Map("map", {
       center: [59.938931, 30.322055],
-      zoom: 17
+      zoom: 17,
+      controls: ['zoomControl']
   });
 
   var mapPlacemark = new ymaps.Placemark([59.938631, 30.323155], {}, {
